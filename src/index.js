b4bb9e850ebd2205dfa38041601d1428ec6ea558
a-florida-key <a.florida.key@gmail.com>
2023-11-24 20:22:38 -0500

    updated forecast JS

--- src/index.js
+++ src/index.js
@@ -75,7 +75,7 @@ function displayForecast() {
                 src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/mist-night.png"
               /></div>
               <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-temperature-max">84°</span>
+                <span class="weather-forecast-temperature-max"><strong>84°</strong></span>
                 <span class="weather-forecast-temperature-min">72°</span>
               </div>
               </div>
